6c1d0999c7f4eb267f24011142261fb8ef35de77
sai123-sudo <ls0122207@gmail.com>
2026-04-08 07:06:08 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 $PERSON1=Sethu
-$PPERSON2=Sai
+$PERSON2=Sai
  
  echo  "PERSON1 : Hi , bro how are you?"
- echo  "sai   : Iam fine bro"
- echo  "PERSON1 : Suggest some movies sai , I have long weekend next week."
- echo  "sai   : Why movies every week, bro? This week, try a web series!"
+ echo  "PERSON   : Iam fine bro"
+ echo  "PERSON1 : Suggest some movies PERSON , I have long weekend next week."
+ echo  "PERSON   : Why movies every week, bro? This week, try a web series!"
  echo  "PERSON1 : which one?"
- echo  "sai   : prision break"
+ echo  "PERSON   : prision break"
  echo  "PERSON1 : Ok , i'll watch that series , Thank you."
