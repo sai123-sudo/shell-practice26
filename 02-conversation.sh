8b4246df29e56c377c2d6a436b93c167d9b0cde3
sai123-sudo <ls0122207@gmail.com>
2026-04-08 07:00:56 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,3 +2,8 @@
  
  echo  "Sethu : Hi , bro how are you?"
  echo  "sai   : Iam fine bro"
+ echo  "Sethu : Suggest some movies sai , I have long weekend next week."
+ echo  "sai   : Why movies every week, bro? This week, try a web series!"
+ echo  "Sethu : which one?"
+ echo  "sai   : prision break"
+ echo  "Sethu : Ok , i'll watch that series , Thank you."
