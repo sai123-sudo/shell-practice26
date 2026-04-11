7fe98363b96764025c3d38515e6e71a7d55e066b
sai123-sudo <ls0122207@gmail.com>
2026-04-11 18:27:22 +0530

    special vars

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,20 +10,24 @@ else
     echo "You are running with root access"
 fi
 
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "Installing $2 is ... SUCESS"
+    else 
+        echo "Installing $2 is ... FAILURE"
+        exit 1
+    fi
+}
+
 dnf list install mysql
 if [ $? -ne 0 ]
 then 
     echo "mysql is not installed... going to install it"
     dnf install mysql -y
-    if [ $? -eq 0 ]
-    then 
-        echo "Installing mysql is ... SUCESS"
-    else 
-        echo "Installing mysql is ... FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "mysql"
 else 
-   echo "mysql is already installed... Nothing to do"
+   echo "MySql is already installed... Nothing to do"
 fi
 
 dnf list install python3
@@ -31,13 +35,7 @@ if [ $? -ne 0 ]
 then 
     echo "python is not installed... going to install it"
     dnf install python -y
-    if [ $? -eq 0 ]
-    then 
-        echo "Installing python is ... SUCESS"
-    else 
-        echo "Installing python is ... FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "python3"
 else 
    echo "python3 is already installed... Nothing to do"
 fi
@@ -47,13 +45,7 @@ if [ $? -ne 0 ]
 then 
     echo "nginx is not installed... going to install it"
     dnf install nginx -y
-    if [ $? -eq 0 ]
-    then 
-        echo "Installing nginx is ... SUCESS"
-    else 
-        echo "Installing nginx is ... FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "nginx"
 else 
    echo "nginx is already installed... Nothing to do"
 fi
