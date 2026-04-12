ee290b38992458960a0f13eb247d31e5baed9e03
sai123-sudo <ls0122207@gmail.com>
2026-04-12 23:11:24 +0530

    special vars

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,6 +1,6 @@
 #1/bin/bash
 
-echo -e "\e[31m hello world"
-echo -e "\e[32m hello world"
+echo -e "\e[31m hello world \e[0m"
+echo -e "hello world"
 echo -e "\e[33m hello world"
 echo -e "\e[34m hello world"
