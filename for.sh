b3e7340f3b55d2cd1a3d095556804d6cce855e2e
sai123-sudo <ls0122207@gmail.com>
2026-04-15 16:26:39 +0530

    colors

--- for.sh
+++ for.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-for i in {1..10}
-
-
+for i in {1..5}
 do  
    echo -e "\e[33m $i  \e[0m"  
 done
 
-for s in {11..15}
+for f in {6..10}
+do  
+   echo -e "\e[34m $f  \e[0m"  
+done
 
+for s in {11..15}
 do  
    echo -e "\e[35m $s  \e[0m"
 done
